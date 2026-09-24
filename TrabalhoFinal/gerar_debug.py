d64e73aee179e0ee56663715dcc044dabc9e7d7f
Arthur Correia Brígido <chrolloacb@gmail.com>
2026-09-24 02:38:25 -0300

    Update gerar_debug.py

--- TrabalhoFinal/gerar_debug.py
+++ TrabalhoFinal/gerar_debug.py
@@ -1,7 +1,9 @@
+# Debug criado separado para teste de geração de arquivos para cada imagem de treino, podendo ser rodado à parte.
+
 import glob
 from shape_pipeline import extract_shape_signature
  
-# ajuste o caminho abaixo se necessario
+# ajuste o caminho abaixo se necessário
 PASTA_TREINO = r"data\train"  # ou caminho absoluto, ex.: r"C:\Users\Arthur\Desktop\TrabalhoFinal\data\train"
  
 for fp in glob.glob(f"{PASTA_TREINO}\\**\\*.jpeg", recursive=True):
